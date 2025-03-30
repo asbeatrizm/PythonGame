d78b7eda16770d90eb598ebce2d80ccd7cee59ab
AnaBM <absmedeiros36@gmail.com>
2025-03-30 01:47:49 -0300

    Creating menu interations Creating ABC class Creating Factory Bg implementation : parallax

--- code/Const.py
+++ code/Const.py
@@ -3,8 +3,19 @@
 COLOR_ORANGE = (255,128,0)
 COLOR_WHITE = (255,255, 255)
 COLOR_BLUE = (18, 10, 143)
+COLOR_YELLOW = (255, 255, 0)
 
 
+# E
+ENTITY_SPEED = {
+    'Level1Bg0' : 0,
+    'Level1Bg1' : 1,
+    'Level1Bg2' : 2,
+    'Level1Bg3' : 3,
+    'Level1Bg4' : 4,
+    'Level1Bg5' : 5,
+    'Level1Bg6' : 6,
+}
 
 # W
 WIN_WIDTH = 576
--- code/Menu.py
+++ code/Menu.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from idlelib.mainmenu import menudefs
+
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
 from code.Const import WIN_WIDTH,MENU_OPTION
-
-
-
 
 class Menu:
     def __init__(self, window):
@@ -16,16 +15,20 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
+            # draw images
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Fun", text_color=(18, 10, 143), text_center_pos=((WIN_WIDTH /2),70))
             self.menu_text(50, "Mountain", text_color=(18, 10, 143),text_center_pos=((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], text_color=(255, 255, 255), text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
-
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], text_color=(255, 255, 0), text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], text_color=(255, 255, 255), text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
             pygame.display.flip()
 
             # Check for all events
@@ -33,6 +36,21 @@ class Menu:
                 if event.type == pygame.QUIT:
                    pygame.quit()  # close window
                    quit()  # end pygame
+                if event.type == pygame.KEYDOWN: # DOWN
+                    if event.key == pygame.K_DOWN:
+                       if menu_option < len(MENU_OPTION) -1:
+                           menu_option += 1
+                       else:
+                           menu_option =0
+                    if event.key == pygame.K_UP: # UP
+                       if menu_option > 0:
+                           menu_option -= 1
+                       else:
+                        menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN: # ENTER
+                        return MENU_OPTION[menu_option]
+
+            #pygame.display.flip()
 
     def menu_text(self,text_size: int, text: str, text_color: tuple, text_center_pos:tuple):
         text_font: Font = pygame.font.SysFont(name="lucida Sans Typewrite", size=text_size)
